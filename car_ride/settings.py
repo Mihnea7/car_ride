d92b37d9a5faf1651fff67d065267dc9f49245c6
Denica Nedjalkova <2298079n@student.gla.ac.uk>
2019-03-03 04:52:42 +0000

    Progress on base.html - not finished yet

--- car_ride/settings.py
+++ car_ride/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# a list of paths with which Django can expect to find static files that can be served
+STATICFILES_DIRS = [STATIC_DIR, ]
