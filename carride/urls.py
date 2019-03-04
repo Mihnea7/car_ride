ec997d7e40843a0ac5e9fb5b5ac6e984698c75db
Shourya Verma <2308432v@student.gla.ac.uk>
2019-03-04 15:27:44 +0000

    lot done

--- carride/urls.py
+++ carride/urls.py
@@ -3,12 +3,16 @@ from carride import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^about/', views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^register/$',views.register,name='register'),
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^myaccount/', views.MyAccount, name='myaccount'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^cardetails/', views.show_car_details, name='cardetails'),
+    #url(r'^buy/', views.buy, name='buy'),
+    #url(r'^rent/', views.rent, name='rent'),
+    #url(r'^sell_lease/', views.sell_lease, name='sell_lease'),
+    #url(r'^compare/', views.compare, name='compare'),
     ]
 
 
