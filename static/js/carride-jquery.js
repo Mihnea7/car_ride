39a0e24b2ada3c15ff2c08c654b119df4dd6cd56
Alexander Cowan <2326127C@student.gla.ac.uk>
2019-03-21 16:38:24 +0000

    buttons

--- static/js/carride-jquery.js
+++ static/js/carride-jquery.js
@@ -1,9 +1,6 @@
 $(document).ready( function() {
 	$("#vehicle_form").hide();
-	$("#z_pic").hide();
-	$("#m_pic").hide();
-	$("#s_pic").hide();
-	$("#d_pic").hide();
+	
 	$("#slide").hide();
 	$("#account_email").hide();
 	$("#account_name").hide();
@@ -12,18 +9,8 @@ $(document).ready( function() {
 	$("#open_sell").click( function(event) {
 		$("#vehicle_form").slideToggle(500);
 	});
-	$("#zander").hover( function(event) {
-		$("#z_pic").fadeIn(2000);
-	});
-	$("#deni").hover( function(event) {
-		$("#d_pic").fadeIn(2000);
-	});
-	$("#sho").hover( function(event) {
-		$("#s_pic").fadeIn(2000);
-	});
-	$("#micc").hover( function(event) {
-		$("#m_pic").fadeIn(2000);
-	});
+	
+	
 	$("#picture").click( function(event) {
 		$("#account_name").slideToggle(500);
 		$("#account_email").slideToggle(500);
